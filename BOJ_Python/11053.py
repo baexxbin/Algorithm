6dab499d7ed9fa10640c22029822a8d5f3578f36
baexxbin <soo3886@gmail.com>
2023-05-22 00:32:57 +0900

    solve: 가장 긴 증가하는 부분 수열
    # dp

---
+++ BOJ_Python/11053.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+if __name__ == '__main__':
+    n = int(input())
+    lst = list(map(int, input().split()))
+    dp = [1]*n
+
+    for i in range(n):
+        for j in range(i):
+            if lst[i] > lst[j]:
+                dp[i] = max(dp[i], dp[j]+1)
+    print(max(dp))
